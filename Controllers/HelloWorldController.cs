ad3e694a5868fa53a76297e4086477aabe25f845
piszczel24 <s26479@pjwstk.edu.pl>
2024-06-30 18:55:11 +0200

    Dodano kontroler

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers;
+
+public class HelloWorldController : Controller
+{
+    //
+    // GET: /HelloWorld/
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    //
+    // GET: /HelloWorld/Welcome/
+    // Requires using System.Text.Encodings.Web;
+    public string Welcome(string name, int ID = 5)
+    {
+        return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+    }
+}
